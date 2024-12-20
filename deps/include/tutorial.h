f4ad8f49f48e1dfccb4014d6f23e88bef5a7ec2c
CHMOSE023 <52737385+CHMOSE023@users.noreply.github.com>
2024-12-20 17:38:51 +0800

    Shader_modules

--- deps/include/tutorial.h
+++ deps/include/tutorial.h
@@ -2,14 +2,17 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
 #include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
 #include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <limits>
 #include <optional>
 #include <set>
-#include <algorithm>// std::clamp
+
 #pragma comment(lib,"glfw3.lib")
 #pragma comment(lib,"vulkan-1.lib")
