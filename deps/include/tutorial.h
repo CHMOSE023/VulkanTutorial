4ec6d0ed207f7875661131858870ab2868a2ad46
CHMOSE023 <52737385+CHMOSE023@users.noreply.github.com>
2024-12-23 10:18:03 +0800

    Texture mapping 纹理映射

--- deps/include/tutorial.h
+++ deps/include/tutorial.h
@@ -1,6 +1,8 @@
 #pragma once
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
